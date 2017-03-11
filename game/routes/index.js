f0a89feee4b5e264f4842b5c7cf1e7df6c335215
visaruth <megasama.one1@gmail.com>
2017-03-11 13:05:00 +0700

    alpha v2.12
    แก้ขนาดรูป
    แก้ชื่อไฟล์ game -> selectBoy

--- game/routes/index.js
+++ game/routes/index.js
@@ -6,12 +6,21 @@ router.get('/', function(req, res, next) {
     res.render('index2');
 });
 
-router.get('/game', function(req, res, next) {
-    res.render('game');
+router.get('/selectBoy', function(req, res, next) {
+    res.render('selectBoy');
 });
 
 router.get('/bio', function(req, res, next) {
     res.render('bio');
+    if (req.query.name == 'in') {
+
+    } else if (req.query.name == 'gr') {
+
+    } else if (req.query.name == 'ph') {
+
+    } else {
+        console.log('404');
+    }
 });
 
 module.exports = router;
