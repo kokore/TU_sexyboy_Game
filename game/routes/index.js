0eca607fd9303d73486d368a8521a66bf75b7d50
visaruth <megasama.one1@gmail.com>
2017-03-11 19:01:20 +0700

    alpha v2.13

--- game/routes/index.js
+++ game/routes/index.js
@@ -10,32 +10,32 @@ router.get('/selectBoy', function(req, res, next) {
     res.render('selectBoy');
 });
 
-router.get('/bio:name', function(req, res, next) {
-    if (req.query.name == 'in') {
-
-        var fs = require("fs");
-        var obj = JSON.parse(fs.readFileSync("public/bio/bioin.json", "utf8"));
-        res.render('bio', {
-            bio: 'obj.bio'
-        });
-
-    } else if (req.query.name == 'gr') {
-
-        var fs = require("fs");
-        var obj = JSON.parse(fs.readFileSync("public/bio/biogr.json", "utf8"));
-        res.render('bio', {
-            bio: 'obj.bio'
-        });
-    } else if (req.query.name == 'ph') {
-
-        var fs = require("fs");
-        var obj = JSON.parse(fs.readFileSync("public/bio/bioph.json", "utf8"));
-        res.render('bio', {
-            bio: 'obj.bio'
-        });
-    } else {
-        console.log('404');
-    }
+router.get('/bio', function(req, res, next) {
+    // if (req.query.name == 'in') {
+    //
+    //     var fs = require("fs");
+    //     var obj = JSON.parse(fs.readFileSync("public/bio/bioin.json", "utf8"));
+    //     res.render('bio', {
+    //         bio: 'obj.bio'
+    //     });
+    //
+    // } else if (req.query.name == 'gr') {
+    //
+    //     var fs = require("fs");
+    //     var obj = JSON.parse(fs.readFileSync("public/bio/biogr.json", "utf8"));
+    //     res.render('bio', {
+    //         bio: 'obj.bio'
+    //     });
+    // } else if (req.query.name == 'ph') {
+    //
+    //     var fs = require("fs");
+    //     var obj = JSON.parse(fs.readFileSync("public/bio/bioph.json", "utf8"));
+    //     res.render('bio', {
+    //         bio: 'obj.bio'
+    //     });
+    // } else {
+    //     console.log('404');
+    // }
 });
 router.get('/question', function(req, res, next) {
     var fs = require('fs');
@@ -48,19 +48,6 @@ router.get('/question', function(req, res, next) {
     var ch2 = jsonformat.question[ch].choice[1];
     var ch3 = jsonformat.question[ch].choice[2];
     var ch4 = jsonformat.question[ch].choice[3];
-    // if (req.query.name == 'in') {
-    //     // var data1 = fs.readFileSync("question/p.in.json",
-    //     //     "utf8");
-    //     // console.log(data);
-    //     console.log('in');
-    //
-    // } else if (req.query.name == 'gr') {
-    //
-    // } else if (req.query.name == 'ph') {
-    //
-    // } else {
-    //     console.log('404');
-    // }
     res.render('question', {
         dataq: question,
         c1: ch1,
