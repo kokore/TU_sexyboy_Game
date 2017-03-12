72e5dc8d181341349a84162359ce01a8839aa8c4
visaruth <megasama.one1@gmail.com>
2017-03-12 12:02:44 +0700

    alpha v3.00

--- game/routes/index.js
+++ game/routes/index.js
@@ -62,8 +62,42 @@ router.get('/question', function(req, res, next) {
         c4: ch4
     });
 });
-router.post('/result', function(req, res, next) {
-    console.log(req.body);
-    res.render('result');
+router.get('/result', function(req, res, next) {
+    var name = req.query.name;
+    var fs = require('fs');
+    var readfromfile = fs.readFileSync('public/question/p.' + name + '.json', 'utf8');
+    var jsonformat = JSON.parse(readfromfile);
+    var score = 0;
+    console.log(jsonformat.question[0].answerquestion + req.query.c1);
+    console.log(jsonformat.question[1].answerquestion + req.query.c2);
+    console.log(jsonformat.question[2].answerquestion + req.query.c3);
+    console.log(jsonformat.question[3].answerquestion + req.query.c4);
+    console.log(jsonformat.question[4].answerquestion + req.query.c5);
+    if (jsonformat.question[0].answerquestion == req.query.c1) {
+        score++;
+    }
+    if (jsonformat.question[1].answerquestion == req.query.c2) {
+        score++;
+    }
+    if (jsonformat.question[2].answerquestion == req.query.c3) {
+        score++;
+    }
+    if (jsonformat.question[3].answerquestion == req.query.c4) {
+        score++;
+    }
+    if (jsonformat.question[4].answerquestion == req.query.c5) {
+        score++;
+    }
+    var result;
+    if (score >= 3) {
+        result = "WIN";
+    } else {
+        result = "LOST";
+    }
+    res.render('result', {
+        name: name,
+        score: score,
+        result: result
+    });
 });
 module.exports = router;
